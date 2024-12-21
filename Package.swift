6a25eb14361cb890b931b24ed54e5b12a5c3614f
yfedchenkopax8c <yuriyfedchenko@gmail.com>
2024-12-22 01:42:27 +0200

    set static lib

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DynamicBorder",
+            type: .static,
             targets: ["DynamicBorder"]),
     ],
     targets: [
