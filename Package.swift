dcd8e3c015529793bc74f163a008813b84bd9fa7
yfedchenkopax8c <yuriyfedchenko@gmail.com>
2024-12-22 08:31:23 +0200

    remove explicit setup of static lib in package

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DynamicBorder",
-            type: .static,
             targets: ["DynamicBorder"]),
     ],
     targets: [
